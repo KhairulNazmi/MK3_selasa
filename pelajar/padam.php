700c5bec4ec72636b10c392c3a73309827c21e98
iqbalhakim705 <143645245+iqbalhakim705@users.noreply.github.com>
2023-11-09 16:35:33 +0800

    Delete pelajar directory

--- pelajar/padam.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../conn.php';
-
-$idperalatan = $_GET['idperalatan'];
-$sql = "DELETE FROM peralatan where idperalatan = $idperalatan";
-$conn->query($sql);
-
-header('location:index.php?menu=pelajar');
